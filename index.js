05ed5f0722ab20fb52fe1322954ac6b8f2346443
hoabuiyt <hoabuiyt@gmail.com>
2024-11-07 22:03:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ async function main() {
   const config = new Config();
   const bot = new Bot(config);
 
-  const proxySource = await selectProxySource(inquirer);
+  //const proxySource = await selectProxySource(inquirer);
+  var proxySource = { 'type': 'none' };
 
   let proxies = [];
   if (proxySource.type === 'file') {
